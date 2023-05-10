2d1297fc75eb4ddbac6d83e9fffd648a4866c7ce
Coymw1 <coymw1@ad.unc.edu>
2023-05-09 20:04:27 -0400

    fixing output

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -37,7 +37,7 @@ if (args.r || args.rules) {
 
 var i = args._[0];
 try {
-    console.log(rpsls(shot))
+    console.log(toString(rpsls(shot)))
 } catch {
     console.log(`Usage: node-rps [SHOT]
     Play Rock Paper Scissors (RPS)
